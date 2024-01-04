b0978d6d70bd8591a1b3cac0292f5119302b480f
motii <108280115+motii8128@users.noreply.github.com>
2024-01-04 17:31:52 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,12 @@ use async_std::{self, io::ReadExt};
 
 use serde_json;
 
-pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
+pub async fn wasd_controller(node_name:&str, yaml_path:&str)->Result<(), Error>
 {
     let session = zenoh::open(Config::default()).res().await.unwrap();
 
-    let bin_name = "wasd_controller";
-
-    let pub_topic = param::get_str_param(yaml_path, bin_name, "pub_topic", "cmd_vel".to_string());
-    let rate = param::get_f64_param(yaml_path, bin_name, "power_rate", 1.0) as f32;
+    let pub_topic = param::get_str_param(yaml_path, node_name, "pub_topic", "cmd_vel".to_string());
+    let rate = param::get_f64_param(yaml_path, node_name, "power_rate", 1.0) as f32;
 
     let publisher = session.declare_publisher(pub_topic).res().await.unwrap();
 
@@ -25,8 +23,7 @@ pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
     let mut input = [0_u8];
 
     let msg = format!("Start pub:{}", publisher.key_expr().to_string());
-
-    logger::log_info(bin_name, msg);
+    logger::log_info(node_name, msg);
 
     loop {
         let _ = stdin.read_exact(&mut input).await.unwrap();
@@ -42,7 +39,7 @@ pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
                 let serialized = serde_json::to_string(&cmd).unwrap();
 
                 let msg = format!("send :{}", serialized);
-                logger::log_info(bin_name, msg);
+                logger::log_info(node_name, msg);
 
                 publisher.put(serialized).res().await.unwrap();
             },
@@ -56,7 +53,7 @@ pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
                 let serialized = serde_json::to_string(&cmd).unwrap();
 
                 let msg = format!("send :{}", serialized);
-                logger::log_info(bin_name, msg);
+                logger::log_info(node_name, msg);
 
                 publisher.put(serialized).res().await.unwrap();
             },
@@ -70,7 +67,7 @@ pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
                 let serialized = serde_json::to_string(&cmd).unwrap();
 
                 let msg = format!("send :{}", serialized);
-                logger::log_info(bin_name, msg);
+                logger::log_info(node_name, msg);
 
                 publisher.put(serialized).res().await.unwrap();
             }
@@ -84,7 +81,7 @@ pub async fn wasd_controller(yaml_path:&str)->Result<(), Error>
                 let serialized = serde_json::to_string(&cmd).unwrap();
 
                 let msg = format!("send :{}", serialized);
-                logger::log_info(bin_name, msg);
+                logger::log_info(node_name, msg);
 
                 publisher.put(serialized).res().await.unwrap();
             }
